039786302d66c3583b2259edd655aeaff866a05e
ij2872 <ijaimes2872@gmail.com>
2019-02-17 03:24:47 -0600

    added data to json file. bundles page works

--- bundleapp/src/Components/BundlePage/index.js
+++ bundleapp/src/Components/BundlePage/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Bundle  from '../Bundle'
+let Bundles  = require('../../data/bundles').bundles;
+
 
 class BundlePage extends Component {
     constructor(props) {
@@ -8,16 +10,36 @@ class BundlePage extends Component {
 
         };
     }
-
+    
+    
     render() {
+        let bundleContent = [];
+
+        for(let i=0; i<Bundles.length; i++){
+            let content = Bundles[i].bundle;
+            bundleContent.push(
+            <Bundle
+                bundleImg={content.img}   
+                title={content.title}
+                author={content.author}
+                products={content.products}
+                description={content.description}
+
+            
+            >
+            
+            </Bundle>);
+        }
+
         return(
             <div>
                 <h1>Bundles</h1>
+                {bundleContent}
+                {/* <Bundle></Bundle>
                 <Bundle></Bundle>
                 <Bundle></Bundle>
                 <Bundle></Bundle>
-                <Bundle></Bundle>
-                <Bundle></Bundle>
+                <Bundle></Bundle> */}
             </div>
         )
     }

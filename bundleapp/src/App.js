127669ec9f2a55a753ed3df5c7922d8714ae55d9
ij2872 <ijaimes2872@gmail.com>
2019-02-16 21:01:13 -0600

    Added home component

--- bundleapp/src/App.js
+++ bundleapp/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Bundle } from './Components';
+import Home from './Components/Home'
+
 
 
 
@@ -10,24 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Bundle>
-          <button><span>[----] </span>Buy</button>
-          <button><span>[----] </span>Info</button>
-        </Bundle>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
+        <Home></Home>
       </div>
     );
   }
---
+++ bundleapp/src/Components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = (props) => {
+    return (
+        <div>
+            <p>Stuff to display</p>
+        </div>
+
+
+    );
+}
+
+export default Home;
